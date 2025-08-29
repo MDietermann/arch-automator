b355e6f8258ca96f15f3065b1383b2b0332a4c1d
MDietermann <mdietermann253@gmail.com>
2025-08-29 20:09:49 +0200

    Added yay and grub theme install

--- install.sh
+++ install.sh
@@ -2,33 +2,57 @@
 
 echo "Initialising Custom Arch Install"
 sudo pacman -Syu
-sudo pacman -S yay
-echo ""
+echo "-------------------------"
+
+echo "Creating required folders"
+mkdir ~/git-clones/
+echo "-------------------------"
 
 echo "Installing basics"
 sudo pacman -S git tmux doppler pass stow
 sudo pacman -S pipewire wireplumber pipewire-audio pipewire-pulse
-echo ""
+echo "-------------------------"
+
+echo "Loading needed Git Repositories"
+cd ~/git-clones/
+git clone https://aur.archlinux.org/yay-bin.git ~/git-clones/yay-bin
+git clone https://github.com/MDietermann/NvimConfig.git ~/git-clones/nvim
+git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/git-clones/Arch-Hyprland
+git clone https://gitlab.com/perthshiretim/grubby-terminal.git
+cd ~
+echo "-------------------------"
+
+echo "Installing yay"
+cd ~/git-clones/yay-bin
+sudo pacman -S base-devel
+makepkg -si
+cd ~
+echo "-------------------------"
 
 echo "Installing Keeper Password manager"
 yay pacman -S keeper-password-manager
-echo ""
+echo "-------------------------"
 
 echo "Installing Nvim"
 sudo pacman -S nvim
-git clone https://github.com/MDietermann/NvimConfig.git ~/git-clones/nvim
 sudo ln -sf ~/git-clones/nvim ~/.config/nvim
-echo ""
+echo "-------------------------"
 
 echo "Installing yazi file manager"
 sudo pacman -S yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick
-echo ""
+echo "-------------------------"
 
 echo "Installing Hyprland with Custom Dotfiles"
-mkdir git-clones
-cd git-clones
-git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/git-clones/Arch-Hyprland
 cd ~/git-clones/Arch-Hyprland
 chmod +x install.sh
 ./install.sh
-echo ""
+echo "-------------------------"
+
+echo "Installing Grub Theme"
+cd ~/git-clones/grubby-terminal
+chmod -x install.sh
+sudo sh install.sh
+sudo grub-mkconfig
+echo "-------------------------"
+
+echo "All done!"
